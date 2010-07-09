361465cf88950766281ce2a7b7ff64e24919de5a
Tom Wright <tom.tdw@gmail.com>
2010-07-09 01:47:49 +0100

    fixed naming conflict

--- ntypes.py
+++ ntypes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3.1
 
 from fractions import Fraction
+from decimal import Decimal
 
 class nint(int):
     def __truediv__(a, b):
         if a % b == 0:
             return a // b
         else:
-            return Fraction(a,b)
+            return Decimal(a) / Decimal(b)
+        #    return Fraction(a,b)
