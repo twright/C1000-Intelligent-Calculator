4d92c39658b6f13b2af15fc125b4978b4fce2f49
Tom Wright <tom.tdw@gmail.com>
2010-11-28 23:44:58 +0000

    Added some missing files

---
+++ cas/statistics.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3.1
+''' This module implements various methods for statistics 
+'''
+__author__ = 'Thomas Wright <tom.tdw@gmail.com>'
+
+from dmath import e, pi
+from .core import handle_type as ht
+from .numerical_methods import romberg_integral
+e = ht(e()); pi = ht(pi())
+
+#def math(x):
+#    import dmath; import math
+#    return dmath if isinstance(x, Decimal) else math
+
+def factorial(x):
+    if x < 0 or not isinstance(x, int):
+        raise ValueError('You cannot calculate the factorial of a negative number')
+    ans = x.__class__(1)
+    while x > 0:
+        ans *= x
+        x -= 1
+    return ans
+    # return x * factorial(x - 1) if x > 0 else 1
+
+def nCr(n, r):
+    return factorial(n) / ( factorial(r) * factorial(n - r) )
+    
+def nPr(n, r):
+    return factorial(n) / factorial(r)
+    
+def binomialpdf(n, p, r):
+    return nCr(n,r) * p**r * (n.__class__(1)-p)**(n-r)
+
+def binomialcdf(n, p, r):
+    if r < 0:
+        return ht(0)
+    if r >= 0:
+        return binomialpdf(n, p, r) + binomialcdf(n, p, r - r.__class__(1))
+
+def poissonpdf(t, r):
+    return e**(-t) * t**r / factorial(r)
+    
+def poissoncdf(t, r):
+    if r < 0:
+        return r.__class__(0)
+    if r >= 0:
+        return poissonpdf(t, r) + poissoncdf(t, r - r.__class__(1))
+        
+def normalcdf(x):
+    f = lambda x: (2*pi*e**(x**2))**(-1/2)
+    return ht(0.5 + romberg_integral(f, 0, x, 5,5))
---
+++ cas/tests/test_numeric.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3.1
+''' Tests for the core funcitonality of the cas. '''
+__author__ = 'Tom Wright <tom.tdw@gmail.com>'
+
+import py.test
+from decimal import Decimal, getcontext, localcontext
+from functools import reduce
+from operator import mul
+
+from cas.numeric import *
+
+class TestComplex():
+    def test_str(self):
+        xs = (
+            (0+0j, '0'), (0.00001+0j, '0'), (-0.00001+0j, '0'), (1+0j, '1'),
+            (1.342+0j, '1.34'), (1-0.01j, '1-0.0100i'), (4.3+0.000003j, '43/10'),
+            (0-2j, '-2i'), (0.000003+0.000002j, '0'), (0.000002-0.000001j, '0'),
+            (4.5+0.000002j, '9/2'), (0.000003-0.2j, '-(1/5)i')
+        )
+        with localcontext():
+            getcontext().prec = 3
+            for x, string in xs:
+                assert str(Complex(x)) == string
+
+    def test_addition(self):
+        assert isinstance(Complex(3+2j) + Complex(-1+5j), Complex)
+
+class TestInteger():
+    def test_init(self):
+        assert Integer(3) == 3
+
+    def test_division(self):
+        assert Integer(4) / Integer(2) == Integer(2)
+        assert Integer(3) / Integer(2) == Decimal('1.5')
+        assert abs(Integer(2) / Decimal('1.1') - Decimal('1.82'))\
+            < Decimal('0.01')
+        with py.test.raises(ZeroDivisionError):
+            Integer(3) / Integer(0)
+
+    def test_addition(self):
+        assert isinstance(Integer(3) + Integer(2), Integer)
+        assert isinstance(Integer(3) + 2, Integer)
+        assert Integer(3) + Integer(2) == 5
+        assert Integer(2) + Decimal('0.01') == Decimal('2.01')
+
+    def test_subtraction(self):
+        assert isinstance(Integer(3) - Integer(2), Integer)
+        assert isinstance(Integer(3) - 2, Integer)
+        assert isinstance(3 - Integer(2), Integer)
+        assert 3 - Integer(2) == 1
+        assert Integer(3) - Integer(2) == 1
+        assert Integer(2) - Decimal('0.01') == Decimal('1.99')
+
+    def test_powers(self):
+        assert isinstance(Integer(2) ** Integer(4), Integer)
+        assert Integer(2) ** Integer(4) == 16
+        assert Integer(4) ** Decimal('0.5') == Decimal('2')
+
+    def test_multiplication(self):
+        assert isinstance(Integer(3) * Integer(2), Integer)
+        assert isinstance(Integer(3) * 2, Integer)
+        assert isinstance(2 * Integer(3), Integer)
+        assert Integer(3) * Integer(2) == 6
+
+    def test_factors(self):
+        xs = (
+            (2, [2]), (4, [2,2]), (10, [2,5]), (-10, [-1,2,5]),
+        )
+        for x, facts in xs:
+            assert list(Integer(x).factors()) == facts
+            assert x == reduce(mul, facts, 1)
